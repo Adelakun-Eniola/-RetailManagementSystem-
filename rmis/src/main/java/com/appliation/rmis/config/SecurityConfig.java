e322e40b9a26e33439db11dd7c3d5cccce2b1feb
boylax23 <eniolaadelakun2005@gmail.com>
2025-07-17 16:38:55 +0100

    security config removed

--- rmis/src/main/java/com/appliation/rmis/config/SecurityConfig.java
+++ rmis/src/main/java/com/appliation/rmis/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.appliation.rmis.config;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
